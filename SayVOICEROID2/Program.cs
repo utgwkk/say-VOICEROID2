4e1685fd928a866e7c26edaebfed0477d26b6d61
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-10-19 19:52:49 +0900

    ひとまず実装完成

--- SayVOICEROID2/Program.cs
+++ SayVOICEROID2/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Windows.Automation;
 
 namespace SayVOICEROID2
 {
@@ -10,6 +7,49 @@ namespace SayVOICEROID2
     {
         static void Main(string[] args)
         {
+            string phrase;
+            if (args.Length < 1)
+                phrase = "引数が指定されていません";
+            else
+                phrase = args[0];
+
+            Say(phrase);
+        }
+
+        static void Say(string phrase)
+        {
+            // VOICEROID2のウィンドウを探す
+            var root = AutomationElement.RootElement;
+            var titleConditions = new [] {
+                new PropertyCondition(AutomationElement.NameProperty, "VOICEROID2"),
+                new PropertyCondition(AutomationElement.NameProperty, "VOICEROID2*")
+            };
+
+            // 喋らせたいフレーズをセットする
+            var form = root.FindFirst(TreeScope.Element | TreeScope.Children, new OrCondition(titleConditions));
+            var textEditView = form.FindFirst(
+                TreeScope.Element | TreeScope.Children,
+                new PropertyCondition(AutomationElement.ClassNameProperty, "TextEditView")
+            );
+            var textBoxElem = textEditView.FindFirst(
+                TreeScope.Element | TreeScope.Children,
+                new PropertyCondition(AutomationElement.ClassNameProperty, "TextBox")
+            );
+            ValuePattern editValue = textBoxElem.GetCurrentPattern(ValuePattern.Pattern) as ValuePattern;
+            editValue.SetValue(phrase);
+
+            // 再生ボタンを探して押す
+            // textEditViewの最初に出てくるボタンが再生ボタンなのでこれで動いているけど将来ずっと動くかは不明
+            var playButton = textEditView.FindFirst(
+                TreeScope.Element | TreeScope.Children,
+                new PropertyCondition(AutomationElement.ClassNameProperty, "Button")
+            );
+            var playButtonControl = playButton.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            playButtonControl.Invoke();
+
+            // 喋っている間はテキストボックスがreadonlyになっているので、喋りおわるまで待つ
+            // Invokeしてすぐはreadonlyになっていなくて抜けてしまうので、do-whileにすることでうまく待つという戦略
+            do { Thread.Sleep(100); } while (editValue.Current.IsReadOnly);
         }
     }
 }
